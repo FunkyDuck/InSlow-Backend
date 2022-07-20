724c267413224a6bf42e1de9e863261bf31c41ec
ginji <jacques_loic@funkyduck.be>
2022-07-20 17:05:09 +0200

    WIP Posts (read bearer Work)

--- src/main/java/tk/inslow/inslowapi/controllers/PostsController.java
+++ src/main/java/tk/inslow/inslowapi/controllers/PostsController.java
@@ -2,10 +2,8 @@ package tk.inslow.inslowapi.controllers;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tk.inslow.inslowapi.config.JwtProvider;
 import tk.inslow.inslowapi.mappers.PostsMapper;
 import tk.inslow.inslowapi.models.dto.PostsDTO;
 import tk.inslow.inslowapi.services.PostsServices;
@@ -20,8 +18,12 @@ public class PostsController
     private PostsMapper postsMapper;
 
     @PostMapping("/posts")
-    public String newPost(@RequestBody PostsDTO postsDTO){
-        System.out.println(postsDTO);
+    public String newPost(@RequestBody ObjectNode object, @RequestHeader(value="Authorization") String header){
+        System.out.println(object);
+        System.out.println(header);
+//        JwtProvider jwtProvider;
+//        jwtProvider.resolveToken();
+
         return "";
     }
 }
